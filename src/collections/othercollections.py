1cbf81b76c6e8c020b5e97c161c74f197255ab72
Touraj <toraj_e@yahoo.com>
2018-10-15 16:34:17 +0200

    explicit conversion!

--- src/collections/othercollections.py
+++ src/collections/othercollections.py
@@ -168,7 +168,31 @@ print("white" not in marbles)
 print("Smith" in surnames.values())
 print("Doe" in surnames.values())
 
+print('-------------------Collection Conversion!----------------')
 
+# [Touraj] :: Explicit conversions between different collections
 
+animals = ['cat', 'dog', 'goldfish', 'canary', 'cat']
 
+animals_set = set(animals)
+animals_unique_list = list(animals_set)
+animals_unique_tuple = tuple(animals_unique_list)
+
+print(animals_set)
+
+marbles = {"red": 34, "green": 30, "brown": 31, "yellow": 29 }
+
+colours = list(marbles) # the keys will be used by default
+counts = tuple(marbles.values()) # but we can use a view to get the values
+marbles_set = set(marbles.items()) # or the key-value pairs
+
+print(colours)
+print(counts)
+print(marbles_set)
+
+# Python doesn't know how to convert this into a dictionary
+# dict([1, 2, 3, 4])
+
+# but this will work
+dict([(1, 2), (3, 4)])
 

482eabc01893a7eae95fd1bdc0666f3dfeaf93d0
Touraj <toraj_e@yahoo.com>
2018-10-15 12:02:39 +0200

    ranges added

--- src/collections/othercollections.py
+++ src/collections/othercollections.py
@@ -82,3 +82,23 @@ a = {}
 
 # this is how we make an empty set
 b = set()
+
+print('---------------------------------Ranges-----------------------------------')
+
+# range is another kind of immutable sequence type. It is very specialised
+# – we use it to create ranges of integers. Ranges are also generators.
+# We will find out more about generators in the next chapter, but for now
+# we just need to know that the numbers in the range are generated one at
+# a time as they are needed, and not all at once. In the examples below,
+# we convert each range to a list so that all the numbers are generated and we can print them out:
+
+# print the integers from 0 to 9
+print(list(range(10)))
+
+# print the integers from 1 to 10
+print(list(range(1, 11)))
+
+# print the odd integers from 1 to 10
+print(list(range(1, 11, 2)))
+
+

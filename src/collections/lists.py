ed12ffbcb67e1508535ad855789402e381df725b
Touraj <toraj_e@yahoo.com>
2018-10-15 10:54:52 +0200

    lists revised

--- src/collections/lists.py
+++ src/collections/lists.py
@@ -30,3 +30,28 @@ print(numbers[-2]) # the second-last element -- 20
 #  Extracting a subset of the list
 print(animals[1:3]) # ['dog', 'fish']
 print(animals[1:-1]) # ['dog', 'fish']
+
+print('----------------------------')
+
+print(animals[2:]) # ['fish', 'bison']
+print(animals[:2]) # ['cat', 'dog']
+print(animals[:]) # a copy of the whole list
+
+# We can even include a third parameter to specify the step size:
+print(animals[::2]) # ['cat', 'fish']
+
+
+# Lists are mutable
+# – we can modify elements, add elements to them or remove elements
+# from them. A list will change size dynamically when we add or remove elements
+# – we don’t have to manage this ourselves:
+
+# assign a new value to an existing element
+animals[3] = "hamster"
+
+# add a new element to the end of the list
+animals.append("squirrel")
+
+# remove an element by its index
+del animals[2]
+

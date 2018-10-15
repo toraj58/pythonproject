3256ada4fe070b3ef5aa534d3eaf6decd06e7cd8
Touraj <toraj_e@yahoo.com>
2018-10-15 10:48:40 +0200

    lists added

---
+++ src/collections/lists.py
@@ -0,0 +1,25 @@
+# a list of strings
+animals = ['cat', 'dog', 'fish', 'bison']
+
+# a list of integers
+numbers = [1, 7, 34, 20, 12]
+
+# an empty list
+my_list = []
+
+one_variable = 'touraj'
+another_variable = 'vitalii'
+third_variable = 'daniel'
+
+# a list of variables we defined somewhere else
+things = [
+    one_variable,
+    another_variable,
+    third_variable, # this trailing comma is legal in Python
+]
+
+print(animals[0]) # cat
+print(numbers[1]) # 7
+
+# This will give us an error, because the list only has four elements
+# print(animals[6])

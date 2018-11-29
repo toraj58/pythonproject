388e3aff1b5d3a04dba1c620c9437bb72d3de52c
Touraj <toraj_e@yahoo.com>
2018-11-29 11:26:53 +0100

    comment on template pattern

--- src/patterns/template.py
+++ src/patterns/template.py
@@ -1,3 +1,11 @@
+# A template pattern defines a basic algorithm in a base class using abstract operation where subclasses override the concrete behavior. The template pattern keeps the outline of algorithm in a separate method. This method is referred as the template method.
+#
+# Following are the different features of the template pattern −
+#
+#     It defines the skeleton of algorithm in an operation
+#
+#     It includes subclasses, which redefine certain steps of an algorithm.
+
 class MakeMeal:
 
     def prepare(self): pass
